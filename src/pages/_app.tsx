413330054ec2c6a86b461a0d049bf748c2b9438a
Shinchan-git <gyoshu.bowie@gmail.com>
2023-02-05 22:32:17 +0900

    fix: Seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import Footer from '@/components/common/Footer/Footer'
 import Header from '@/components/common/Header'
 import Layout from '@/components/common/Layout'
+import Seo from '@/components/common/Seo'
 import '@/styles/global.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Seo />
       <Header />
       <Component {...pageProps} />
       <Footer />
